f2c47deda8ebba945dd694f4db2f054b4d2f1266
inetsos <inetsos@naver.com>
2025-07-22 20:44:24 +0900

    홈 UI 일부 개선

--- app/page.tsx
+++ app/page.tsx
@@ -39,8 +39,8 @@ export default async function Home() {
           </p>
 
           <div className="bg-gray-100 dark:bg-gray-800 p-4 rounded-lg">
-            <h2 className="text-xl font-semibold mb-2">🌿 우리 동네의 숨은 보석, 이제 만나보세요!</h2>
-            <p className="text-gray-700 dark:text-gray-200">
+            <strong className="text-lg">🌿 우리 동네의 숨은 보석, 만나보세요!</strong>
+            <p className="mt-4 text-gray-700 dark:text-gray-200">
               지역 상점과 주민을 이어주는 <strong>시지 라이프</strong>는 골목길 작은 가게부터 정겨운 단골집까지—  
               당신의 일상 속 가까운 곳에서 특별한 경험을 선물합니다. <br />
               이웃과 함께하는 소비, 그 속에 진짜 가치가 있습니다.
@@ -73,29 +73,28 @@ export default async function Home() {
         </div>
       </div>
 
-    <br />
-    <div className="my-2 p-4">
-      <h6 className="text-xl font-bold mb-6">📍 대구광역시 수성구 시지 지역은?</h6>
+      <br />
+      <div className="my-2 p-4">
+        <h6 className="text-xl font-bold mb-6">📍 대구광역시 수성구 시지 지역은?</h6>
 
-      <p className="text-lg leading-relaxed mb-4">
-        맑은 공기와 여유로운 분위기가 흐르는 <strong>시지 지역</strong>은
-        신매동, 욱수동, 사월동, 매호동, 노변동, 시지동 일대를 포괄합니다.
-      </p>
+        <p className="text-lg leading-relaxed mb-4">
+          맑은 공기와 여유로운 분위기가 흐르는 <strong>시지 지역</strong>은
+          신매동, 욱수동, 사월동, 매호동, 노변동, 시지동 일대를 포괄합니다.
+        </p>
 
-      <p className="text-lg leading-relaxed mb-4">
-        넓은 녹지와 생활 인프라가 잘 조화를 이루며,
-        커피 향이 퍼지는 골목, 아이들 웃음소리가 들리는 공원,
-        그리고 도심 속 작은 쉼표 같은 공간들이 가득한 이곳은
-        주거와 문화, 교육까지 모두 갖춘 <strong>젊은 층 선호 1순위 지역</strong>입니다.
-      </p>
+        <p className="text-lg leading-relaxed mb-4">
+          넓은 녹지와 생활 인프라가 잘 조화를 이루며,
+          커피 향이 퍼지는 골목, 아이들 웃음소리가 들리는 공원,
+          그리고 도심 속 작은 쉼표 같은 공간들이 가득한 이곳은
+          주거와 문화, 교육까지 모두 갖춘 <strong>젊은 층 선호 1순위 지역</strong>입니다.
+        </p>
 
-      <p className="text-lg leading-relaxed">
-        하루의 소소한 행복, <br/>
-        그 시작은 바로 우리 동네 <strong>시지 라이프</strong>에서 🌸
-      </p>
-    </div>
-    <br />
-    <br />
+        <p className="text-lg leading-relaxed">
+          하루의 소소한 행복, 🌸 <br/>
+          그 시작은 바로 우리 동네 <strong>시지 라이프</strong>에서 
+        </p>
+      </div>
+
     </div>
   );
 }
